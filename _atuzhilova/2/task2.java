88f15849ef2ba143a01843f3266df9f7afc668ad
atuzhil <alena.tuzhilova@mail.ru>
2019-01-28 16:22:52 +0300

    Lection 2. Task 2

--- _atuzhilova/2/task2.java
+++ _atuzhilova/2/task2.java
@@ -5,13 +5,13 @@ public class task2 {
     public static void main (String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int sum = 0;
-        boolean isMatch1000 = false;
+        boolean isGreater1000 = false;
         for(int number: array) {
             sum += number;
 
-            if(!isMatch1000 && number == 1000) {
+            if(!isGreater1000 && number > 1000) {
                 System.out.println("Массив массивный!");
-                isMatch1000 = true;
+                isGreater1000 = true;
             }
         }
         System.out.println("Сумма элементов массива: " + sum);
