36a1ea041af63734375479cf9ef25ff77f9e9ad5
atarasov <atarasov@mera.ru>
2019-02-04 10:15:41 +0300

    Lesson 6

---
+++ _LECTIONS/7/CollectionsSample.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class CollectionsSample {
+    List lists = new ArrayList();
+    Set sets = new HashSet();
+    Queue queue = new LinkedList();
+    Map maps = new HashMap();
+
+    public static void main(String[] args) {
+        CollectionsSample s = new CollectionsSample();
+
+      /*  s.lists.add("123");
+        s.lists.add("123");
+        System.out.println(s.lists);
+
+
+        s.sets.add("123");
+        s.sets.add("123");
+        System.out.println(s.sets);
+
+        s.maps.put("Андрей","Тарасов");
+        s.maps.put("Андрей","ТарасоВ");
+        s.maps.put("Вася","Петечкин");
+
+        System.out.println(s.maps.get("Андрей"));
+
+        System.out.println(s.maps.keySet());
+        System.out.println(s.maps.values());*/
+
+
+
+
+    }
+}
